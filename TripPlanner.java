ff88338d6744cb20a777d5de12ea7154f0408e2c
Max <17439232+mlarner@users.noreply.github.com>
2019-05-10 09:54:53 -0700

    Update TripPlanner.java

--- TripPlanner.java
+++ TripPlanner.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 
 public class TripPlanner {
   public static void main (String[] args){
@@ -6,12 +6,12 @@ public class TripPlanner {
     greeting();
     travelTime();
     timeDifference();
-    countryArea()
+    countryArea();
   }
   
-  public static void greeting(String[] args){
+  public static void greeting(){
   
-    Scanner input = new scanner(System.in);
+    Scanner input = new Scanner(System.in);
     
     System.out.println("Welcome to vacation planner!");
     System.out.println("What is your name?");
@@ -21,59 +21,63 @@ public class TripPlanner {
     String destination = input.nextline();
     
     System.out.println("Great! " + destination + " sounds like a great trip");
-    System.out.println("**********\n\n")
+    System.out.println("**********\n\n");
   }
   
-  public static void travelTime(String[] args){
-    Scanner input = new scanner(System.in);
+  public static void travelTime(){
+    Scanner input = new Scanner(System.in);
     
     System.out.println("How many days are you going to spend travling?");
     String days = input.nextline();
+    int daysInt = Integer.parseInt(days);
     
     System.out.println("How much money, in usd, are you going to spend on your trip?");
-    String budget = input.nextline();
+    String budget = input.nextline();   
+    int budgetInt = Integer.parseInt(budget);
     
     System.out.println("What is the three letter currency symbol?");
     String currencySymbol = input.nextline();
     
     System.out.println("How many " + currencySymbol + " are there in each dollar?");
     String currencyConversion = input.nextline();
+    double currencyConversionDbl = Double.parseDouble(currencyConversion);
     
-    int hours = days * 24;
+    int hours = daysInt * 24;
     int minutes = hours * 60;
-    double dailyBudget = rounded(budget/days)'
-    double convertedBudget = rounded(budget*currencyConversion);
-    double convertedDaily = rounded(dailyBudget*currencyConversion);
+    double dailyBudget = rounded(budgetInt/daysInt);
+    double convertedBudget = rounded(budgetInt*currencyConversionDlb);
+    double convertedDaily = rounded(dailyBudget * currencyConversionDbl);
     
     System.out.println("If you are traveling for " + days + " that is the same as " + days + " or " + minutes + " minutes.");
     System.out.println("If you are going to spend $" + budget + " that means per day you can spend up to $" + dailyBudget + " USD");
     System.out.println("Your total budget in " + currencySymbol + " is " + currencyConverted + " " + currencySymbol + "which per day is " + convertedDaily + " " + currencySymbol);
     
     System.out.println("**********\n\n");
-
   }
-
-  public staic void timeDifference(String args[]){
-    Scanner input = new scanner(System.in);
+  public static void timeDifference(){
+    Scanner input = new Scanner(System.in);
     
     System.out.println("What is the time difference, in hours, between your home and travel destination?");
     String timeDiff = input.nextline();
+
+    int timeDiffInt = Integer.parseInt(timeDiff);
     
-    int midnightConversion = timeDiff % 24;
+    int midnightConversion = timeDiffInt % 24;
     
-    int noonConversion = (12 + timeDiff) % 24;
+    int noonConversion = (12 + timeDiffInt) % 24;
     
     System.out.println("That means when it is midnight at home it will be " + midnightConversion + ":00 in your travel destination and when it is noon at home it will be " + noonConversion + ":00");
   
   }
   
-  public static void countryArea(String args[]){
+  public static void countryArea(){
   
-  Scanner input = new scanner(system.in);
+  Scanner input = new Scanner(system.in);
   System.out.println("What is the dquare area of your desitnation country in km2?");
   String km2 = input.nextline();
+  int km2Int = Integer.parseInt(km2);
   
-  double mi2 = km2 * 0.386102;
+  double mi2 = km2Int * 0.386102;
   double rounded = round(mi2);
   System.out.println("In miles2 that is " + rounded);
   
@@ -87,6 +91,4 @@ public class TripPlanner {
     
     return rounded;
   }
-  
-  
 }
